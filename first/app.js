457d4ed4ba9999b740f7dd962445ca7f1457f061
patrick <patricklindner@outlook.com>
2021-10-26 10:07:55 +0200

    init

--- first/app.js
+++ first/app.js
@@ -15,10 +15,13 @@ app.get('/', async (req, res) => {
     }
 })
 
-app.use((req, res) => {
-    console.log("404 at", req.url)
-    res.status(404).send("Not found.")
+app.get("/test", (req, res) => {
+    res.send("Hallo test")
+})
 
+app.use((req, res, next) => {
+    console.log("404 at", req.url)
+    res.status(404).send("Not found from express")
 })
 
 app.listen(port, () => {
--- second/app.js
+++ second/app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
     res.send('Hello from second')
 })
 
+app.use((req, res, next) => {
+    console.log("404 at", req.url)
+    res.status(404).send("Not found from second express")
+})
+
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
